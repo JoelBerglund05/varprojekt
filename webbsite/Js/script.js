b0a23b2956a13278c503936a8aae2913b009e467
Ovan812 <simonnilrud@gmail.com>
2023-04-22 16:55:25 +0200

    parallax ish

--- webbsite/Js/script.js
+++ webbsite/Js/script.js
@@ -1,5 +1,22 @@
-// ES6 import
-import simpleParallax from "simple-parallax-js";
+function castParallax() {
+  var opThresh = 350;
+  var opFactor = 750;
 
-// CommonJS
-const simpleParallax = require("simple-parallax-js");
+  window.addEventListener("scroll", function (event) {
+    var top = this.pageYOffset;
+
+    var layers = document.getElementsByClassName("parallax");
+    var layer, speed, yPos;
+    for (var i = 0; i < layers.length; i++) {
+      layer = layers[i];
+      speed = layer.getAttribute("data-speed");
+      var yPos = -((top * speed) / 100);
+      layer.setAttribute(
+        "style",
+        "transform: translate3d(0px, " + yPos + "px, 0px)"
+      );
+    }
+  });
+}
+
+document.body.onload = castParallax();
