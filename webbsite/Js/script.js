95deb203291daa50686b8e77f52300f6a4c01700
Joel Berglund <joelberglund4497@gmail.com>
2023-05-20 16:41:34 +0200

    frsfgrs

--- webbsite/Js/script.js
+++ webbsite/Js/script.js
@@ -1,13 +1,13 @@
 function cast_parallax() {
   window.addEventListener("scroll", function () {
-    var top = this.scrollY;
+    let top = this.scrollY;
 
     const layer_list = document.getElementsByClassName("parallax");
     // Calculates the positioning of each layer
-    for (var i = 0; i < layer_list.length; i++) {
+    for (let i = 0; i < layer_list.length; i++) {
       layer = layer_list[i];
       speed = layer.getAttribute("data-speed");
-      var y_position = -((top * speed) / 100);
+      let y_position = -((top * speed) / 100);
       layer.setAttribute(
         "style",
         "transform: translate3d(0px, " + y_position + "px, 0px);"
@@ -20,12 +20,19 @@ document.body.onload = cast_parallax();
 
 function set_darkmode() {
   if (
-    "0" ==
-    (document.cookie.match(/^(?:.*;)?\s*Theme\s*=\s*([^;]+)(?:.*)?$/) || [
-      ,
-      null,
-    ])[1]
+    "0" ===
+      (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/) || [
+        ,
+        null,
+      ])[1] ||
+    "1" ==
+      (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/) || [
+        ,
+        null,
+      ])[1]
   ) {
+    document.cookie = "Theme=1;";
+  } else {
     let oldlink = document.getElementsByTagName("link").item(1);
 
     let newlink = document.createElement("link");
@@ -37,7 +44,5 @@ function set_darkmode() {
       .getElementsByTagName("head")
       .item(1)
       .replaceChild(newlink, oldlink);
-  } else {
-    document.cookie = "Theme=1;";
   }
 }
